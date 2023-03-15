c0e154b6bf067c6f7974b1a2f656c9ccefcf558e
kylerusinko <64221989+kylerusinko@users.noreply.github.com>
2023-03-15 11:44:54 -0400

    added exceptions

--- webscraper.py
+++ webscraper.py
@@ -7,19 +7,28 @@ url = input("Enter the URL you want to scrape: ")
 keywords = input("Enter the keywords you want to search for, separated by commas: ").split(",")
 
 # Send a GET request to the URL and parse the response with Beautiful Soup
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+try:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+except:
+    print("Error parsing information")
 
 # Find all the paragraphs in the HTML and search for the keywords in them
+
 for paragraph in soup.find_all('p'):
     for keyword in keywords:
-        if keyword.strip().lower() in paragraph.get_text().strip().lower():
-            results = paragraph.get_text().strip()
-            print(f"Keyword '{keyword.strip()}' found in paragraph: {paragraph.get_text().strip()}")
-        with open('webscrape.csv', 'w', newline='') as text_file:
-            writer = csv.writer(text_file)
-            for string in results:
-                writer.writerow([string])
-
-            ## text_file.write(str(keyword.strip()))
+        try:
+            if keyword.strip().lower() in paragraph.get_text().strip().lower():
+                results = paragraph.get_text().strip()
+                print(f"Keyword '{keyword.strip()}' found in paragraph: {paragraph.get_text().strip()}")
+        except:
+            print("Error found when scraping")
+        try:
+            with open('webscrape.csv', 'w', newline='') as text_file:
+                writer = csv.writer(text_file)
+                for string in results:
+                    writer.writerow([string])
+        except:
+            print("Error writing to CSV file")
+                ## text_file.write(str(keyword.strip()))
 
