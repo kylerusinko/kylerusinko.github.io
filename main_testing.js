2ff149ee58ce9b36916cdc81140773c47cad26e3
Kyle Rusinko <kylerusinko78@yahoo.com>
2023-04-23 22:57:30 -0400

    4-23

--- main_testing.js
+++ main_testing.js
@@ -1,5 +1,20 @@
-async function scrapeWebsites(websites, keywords) {
-  const results = [];
+async function getSynonyms(word) {
+  const apiEndpoint = `https://api.datamuse.com/words?rel_syn=${word}`;
+  try {
+    const response = await fetch(apiEndpoint);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const json = await response.json();
+    return json.map(obj => obj.word);
+  } catch (a) {
+    console.log(a);
+    return [];
+  }
+}
+
+async function scrapeIt(websites, keywords) {
+  const resultArray = [];
 
   for (const website of websites) {
     const response = await fetch(website);
@@ -7,7 +22,7 @@ async function scrapeWebsites(websites, keywords) {
     const doc = new DOMParser().parseFromString(html, 'text/html');
 
     const content = doc.body.textContent;
-    const sentences = content.split(/[.;:]/); // split on more punctuation
+    const sentences = content.split(/[.;:]/);
 
     for (const keyword of keywords) {
       let found = false;
@@ -24,7 +39,6 @@ async function scrapeWebsites(websites, keywords) {
           let startIndex = Math.max(0, keywordIndex - 10);
           let endIndex = Math.min(sentenceWords.length, keywordIndex + 11);
 
-          // Stop at punctuation, script tags, or semi-colon/colon
           for (let j = keywordIndex - 1; j >= startIndex; j--) {
             const word = sentenceWords[j];
             if (/^[.;:]$/.test(word) || /^<script/.test(word)) {
@@ -49,37 +63,79 @@ async function scrapeWebsites(websites, keywords) {
         }
       }
 
-      results.push({
+      if (!found) {
+        const synonyms = await getSynonyms(keyword);
+        for (const synonym of synonyms) {
+          for (let i = 0; i < sentences.length; i++) {
+            const sentence = sentences[i];
+            if (sentence.toLowerCase().includes(synonym.toLowerCase())) {
+              found = true;
+              const sentenceWithTag = sentence.replace(new RegExp(`(${synonym})`, 'gi'), '<strong class="synonym">$1</strong>');
+              const sentenceWithoutTag = sentenceWithTag.replace(/(<[^>]+>|&nbsp;)/gi, '');
+              const sentenceWords = sentenceWithoutTag.trim().split(' ');
+              const keywordIndex = sentenceWords.findIndex(word => word.toLowerCase().includes(synonym.toLowerCase()));
+              let startIndex = Math.max(0, keywordIndex - 10);
+              let endIndex = Math.min(sentenceWords.length, keywordIndex + 11);
+
+              for (let j = keywordIndex - 1; j >= startIndex; j--) {
+                const word = sentenceWords[j];
+                if (/^[.;:]$/.test(word) || /^<script/.test(word)) {
+                  startIndex = j + 1;
+                  break;
+                }
+              }
+
+              for (let j = keywordIndex + 1; j < endIndex; j++) {
+                const word = sentenceWords[j];
+                if (/^[.;:]$/.test(word) || /^<\/script/.test(word)) {
+                  endIndex = j;
+                  break;
+                }
+              }
+
+              const shortenedSentence = sentenceWords.slice(startIndex, endIndex).join(' ');
+              sentenceIndices.push({
+                index: i,
+                sentence: shortenedSentence
+              });
+            }
+          }
+
+          if (found) {
+            break;
+          }
+        }
+      }
+
+      resultArray.push({
         website: website,
         keyword: keyword,
         found: found,
         sentenceIndices: sentenceIndices,
-        html: html // store the entire HTML content of the website
+        html: html
       });
     }
   }
 
-
-  return results;
+  return resultArray;
 }
 
-function downloadHTML(website, results) {
+// Adapted from https://stackoverflow.com/questions/72490229/how-to-generate-a-download-file-in-javascript
+function downloadHTML(website, resultArray) {
   const filename = `${website}.html`;
-  const matchingResult = results.find(result => result.website === website);
+  const matchingResult = resultArray.find(result => result.website === website);
 
   if (matchingResult) {
-    const element = document.createElement('a');
-    element.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(matchingResult.html));
-    element.setAttribute('download', filename);
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
+    const htmlElement = document.createElement('a');
+    htmlElement.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(matchingResult.html));
+    htmlElement.setAttribute('download', filename);
+    document.body.appendChild(htmlElement);
+    htmlElement.click();
+    document.body.removeChild(htmlElement);
   } else {
-    console.error(`No matching result found for website '${website}'.`);
+    console.error(`Error: '${website}'.`);
   }
 }
-
 
 const scrapeButton = document.getElementById('scrapeButton');
 const output = document.getElementById('output');
@@ -90,10 +146,10 @@ scrapeButton.addEventListener('click', () => {
   const websites = websitesInput.value.split(',');
   const keywords = keywordsInput.value.split(',');
 
-  scrapeWebsites(websites, keywords).then((results) => {
+  scrapeIt(websites, keywords).then((resultArray) => {
     let html = '';
 
-    for (const result of results) {
+    for (const result of resultArray) {
       const sentenceCount = result.sentenceIndices.length;
       html += `<h3>${result.website} - ${result.keyword} (${sentenceCount} instances found)</h3>`;
       if (result.found) {
@@ -108,7 +164,7 @@ scrapeButton.addEventListener('click', () => {
 
         html += '</ul>';
       } else {
-        html += '<p>Keyword not found.</p>';
+        html += '<p>Keyword/Synonyms not found.</p>';
       }
     }
 
@@ -116,10 +172,10 @@ scrapeButton.addEventListener('click', () => {
     const downloadButton = document.createElement('button');
     downloadButton.textContent = 'Download HTML';
     downloadButton.addEventListener('click', async () => {
-      const results = await scrapeWebsites(websites, keywords);
+      const resultArray = await scrapeIt(websites, keywords);
       let html = '';
-    
-      for (const result of results) {
+
+      for (const result of resultArray) {
         if (result.found) {
           const response = await fetch(result.website);
           const websiteHtml = await response.text();
@@ -132,10 +188,10 @@ scrapeButton.addEventListener('click', () => {
           }).join('');
         }
       }
-    
-      downloadHTML(websites[0], results); // Pass the website and results variables to downloadHTML()
+
+      downloadHTML(websites[0], resultArray);
     });
-    
+
     output.appendChild(downloadButton);
 
 
